2c944428736f843c644c6693399edf3b85fd21ef
Kbow009 <bowman.kristin@gmail.com>
2021-10-08 14:46:37 -0700

    Spring Boot with JPA dependencies

---
+++ WellnessTracker/src/main/java/com/skilldistillery/wellnesstracker/WellnessTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.wellnesstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellnessTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellnessTrackerApplication.class, args);
+	}
+
+}
