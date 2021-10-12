88559631b82ed82e44a04112dfeb27fd57283e00
Kbow009 <bowman.kristin@gmail.com>
2021-10-12 11:38:01 -0700

    war file

--- WellnessTracker/src/main/java/com/skilldistillery/wellnesstracker/WellnessTrackerApplication.java
+++ WellnessTracker/src/main/java/com/skilldistillery/wellnesstracker/WellnessTrackerApplication.java
@@ -2,10 +2,15 @@ package com.skilldistillery.wellnesstracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class WellnessTrackerApplication extends SpringBootServletInitializer {
+	 @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(WellnessTrackerApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(WellnessTrackerApplication.class, args);
